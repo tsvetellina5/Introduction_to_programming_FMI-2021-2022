551bdf93da702b5eff216981591aefd98343a270
Justsvetoslavov <50053338+Justsvetoslavov@users.noreply.github.com>
2021-10-29 00:35:33 +0300

    Update gcd-exercise_example.cpp

--- Sem. 03/Examples/gcd-exercise_example.cpp
+++ Sem. 03/Examples/gcd-exercise_example.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-	int num1, num2; //24,16
+	int num1, num2;
 	cin >> num1 >> num2;
 
-	int gcd = num1 < num2 ? num1 : num2; //16
+	int gcd = num1 < num2 ? num1 : num2;
 
 	while (gcd >= 1)
 	{
