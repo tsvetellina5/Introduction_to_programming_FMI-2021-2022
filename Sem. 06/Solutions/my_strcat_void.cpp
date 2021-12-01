64aa64baca0dcc4b70dd4c7f0958203e50315d79
Justsvetoslavov <justsvetoslavov@gmail.com>
2021-12-01 19:31:50 +0200

    Add sem.07 solutions

---
+++ Sem. 06/Solutions/my_strcat_void.cpp
@@ -0,0 +1,29 @@
+//dest does NOT have enough space, return a new array
+void my_strcat4(char* dest, const char* source)
+{
+	size_t destLength = my_strlen(dest);
+	size_t sourceLength = my_strlen(source);
+	size_t resultLength = destLength + sourceLength + 1;
+
+	char* temp = new char[destLength];
+	for (size_t i = 0; i < destLength; i++)
+	{
+		temp[i] = dest[i];
+	}
+
+	delete[] dest;
+	dest = new char[resultLength];
+
+	for (size_t i = 0; i < destLength; i++)
+	{
+		dest[i] = temp[i];
+	}
+
+	for (size_t y = 0; y < sourceLength; y++)
+	{
+		dest[destLength + y] = source[y];
+	}
+
+	dest[resultLength - 1] = '\0';
+	delete[] temp;
+}
