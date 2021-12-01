81773a9ad48e2cf313292ba8af9357db94c587d7
Justsvetoslavov <justsvetoslavov@gmail.com>
2021-12-01 19:36:37 +0200

    Example MemoryLeak

---
+++ Sem. 07/Example_DynamicArray-Memory.cpp
@@ -0,0 +1,16 @@
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#endif
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	int* arr = new int[1000];
+	int* arr2 = new int();
+	int* arr3 = new int[100];
+	delete[] arr3; 
+}
