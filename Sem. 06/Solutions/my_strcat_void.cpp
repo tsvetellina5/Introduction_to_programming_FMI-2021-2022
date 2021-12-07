bdf6ee19638e67e6d644d67b0de7e285195ae74c
Justsvetoslavov <50053338+Justsvetoslavov@users.noreply.github.com>
2021-12-07 15:36:13 +0200

    Update my_strcat_void.cpp

--- Sem. 06/Solutions/my_strcat_void.cpp
+++ Sem. 06/Solutions/my_strcat_void.cpp
@@ -1,5 +1,14 @@
+unsigned my_strlen(const char *str) {
+	unsigned index = 0;
+
+	while (str[index] != TERMINATE_SYMBOL) {
+		index++;
+	}
+	return index;
+}
+
 //dest does NOT have enough space, return a new array
-void my_strcat4(char* dest, const char* source)
+void my_strcat4(char *&dest, const char* source)
 {
 	size_t destLength = my_strlen(dest);
 	size_t sourceLength = my_strlen(source);
@@ -12,8 +21,10 @@ void my_strcat4(char* dest, const char* source)
 	}
 
 	delete[] dest;
+	dest = nullptr;
+	
 	dest = new char[resultLength];
-
+	
 	for (size_t i = 0; i < destLength; i++)
 	{
 		dest[i] = temp[i];
@@ -26,4 +37,5 @@ void my_strcat4(char* dest, const char* source)
 
 	dest[resultLength - 1] = '\0';
 	delete[] temp;
+	temp = nullptr;
 }
