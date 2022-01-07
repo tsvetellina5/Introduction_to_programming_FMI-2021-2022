d139a0e62221c63d011eabd065dc4448ca43e77c
Justsvetoslavov <justsvetoslavov@gmail.com>
2022-01-07 18:30:21 +0200

    Add another solution
    Different ways of implementing Linear and Binary search via recursion.

--- Sem. 08/Solutions/binarySearch.cpp
+++ Sem. 08/Solutions/binarySearch.cpp
@@ -1,3 +1,5 @@
+//binary search - way1
+//with argument size
 bool binarySearch(const int* arr, unsigned size, int num)
 {
 	if (size == 0)
@@ -12,4 +14,23 @@ bool binarySearch(const int* arr, unsigned size, int num)
 		return binarySearch(arr, mid, num);
 	
 	return binarySearch(arr + mid + 1, size - mid - 1, num);
+}
+
+//===========================================================
+
+//binary search - way2
+//int l = left - first element
+//int r = right - last element
+bool binarySearch(const int* arr, int l, int r, int num)
+{
+	int mid = l + (r - 1) / 2;
+	
+	if (arr[mid] == num) //found the desired element
+		return true;
+	
+	if (arr[mid] > num)
+		return binarySearch(arr, l, mid - 1, num);
+	
+	//arr[mid] < num;
+	return binarySearch(arr, mid + 1, r, num);
 }
